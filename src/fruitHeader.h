1abcec4c5ecbebd6d5dfe85379cdade8ab8e5314
yasmine hassan <yasminehassan2621@gmail.com>
2021-06-16 12:04:23 +0200

    final

--- src/fruitHeader.h
+++ src/fruitHeader.h
@@ -1,48 +1,118 @@
- #include <iostream>
+#include <string.h>
+#include <iostream>
+
 
  //base class 
 class Fruit{
-   public:
+   private:
   		 Fruit* leftChild;   
   		 Fruit* rightChild;
 		 int weight;
+         
 
    public:
 		Fruit* getLeft();
 		Fruit* getRight();
         int getWeight();
         void setWeight(int);
+        void setRight(Fruit*);
+        void setLeft(Fruit*);
         Fruit(int);
         Fruit();
+        virtual std::string getTaste();
+        virtual std::string getType();
+        virtual std::string getSpecific();
+
 
 };
-class Tree{
-
+ class Tree{
+ private:
+        Fruit* parent;
  public:
         Fruit* findLightest();
         Fruit* findHeaviest(); 
+        void setParent(Fruit*);
+        Fruit* getParent();
         void Iterate();
+        void IterateHelper(std::string);
         void filterByWeight(int);
-        void magnifyByType(Fruit*,int);
-        void filterByType(Fruit*); 
-        Fruit* Insert(Fruit*, int );
-};
-//subclasses
-class OvalShape : public Fruit{
+        void magnifyByType(std::string,int);
+        void filterByType(std::string); 
+        Fruit* Insert(Fruit*,Fruit*, std::string,int );
+        Fruit* InsertHelper(std::string,int);
+        Fruit* findSuccessor(Fruit*,Fruit*,int);
+        Fruit* searchForNode(Fruit*,Fruit* , int );
+        Fruit* deleteFruit(int);
+       void levelOrder(Fruit* ) ;
 
 };
-class TinyShape : public Fruit{
+//subclasses
+class OvalShape :virtual public Fruit{
+    public:
+        virtual std::string getType();
+        virtual std::string getSpecific();
+
+};
+class TinyShape : virtual public Fruit{
+    public:
+        virtual std::string getType();
+        virtual std::string getSpecific();
+
 
 };
 class Grapes : public TinyShape ,public OvalShape{
+    public:
+        virtual std::string getTaste();
+        virtual std::string getType();
+        virtual std::string getSpecific();
 
 };
 class Berry : public TinyShape,public OvalShape{
+   
+    public:
+        virtual std::string getTaste();
+        virtual std::string getType();
+        virtual std::string getSpecific();
+};
+class BlueBerry : public Berry{
 
+    public:
+        virtual std::string getType();
+        virtual std::string getSpecific();
+};
+class BlackBerry : public Berry{
+
+    public:
+        virtual std::string getType();
+        virtual std::string getSpecific();
+};
+class GooseBerry : public Berry{
+
+    public:
+        virtual std::string getType();
+        virtual std::string getSpecific();
+};
+class ElderBerry : public Berry{
+    public:
+        virtual std::string getType();
+        virtual std::string getSpecific();
+};
+class Strawberry : public Berry{
+
+    public:
+        virtual std::string getTaste();
+        virtual std::string getType();
+        virtual std::string getSpecific();
 };
 class Avocado : public OvalShape{
-
+     public:
+        virtual std::string getTaste();
+        virtual std::string getType();
+        virtual std::string getSpecific();
 };
 class Apple : public OvalShape{
-
+    public:
+        virtual std::string getTaste();
+        virtual std::string getType();
+        virtual std::string getSpecific();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <string.h>
 #include <iostream>
 #include <stack>
 #include <list>
 #include "fruitHeader.h"
 
 using namespace std;
+
 Fruit* root=NULL;
+//fruit 
 Fruit ::Fruit(int value) 
 {
 	 weight = value; 
@@ -14,69 +17,255 @@ Fruit ::Fruit() {
 	 leftChild = rightChild= NULL; } 
 
 Fruit* Fruit::getLeft(){
-	return leftChild;
+	return this->leftChild;
+}
+void Fruit::setLeft(Fruit* value){
+	 this->leftChild=value;
 }
 Fruit* Fruit::getRight(){
 	return rightChild;
+}
+void Fruit::setRight(Fruit* value){
+	 this->rightChild=value;
 }
 int Fruit::getWeight(){
 	return weight;
 }
 void Fruit::setWeight(int w){
-weight=w;
+        this->weight=w;
+}
+string Fruit::getTaste(){
+	return "I have multiple tastes";
+}
+string Fruit::getType(){
+	return "Fruit";
+}
+string Fruit::getSpecific(){
+	return "Fruit";
+}
+//subclasses
+string OvalShape::getType(){
+	return "Fruit OvalShape";
+}
+string OvalShape::getSpecific(){
+	return "OvalShape";
+}
+string TinyShape::getType(){
+	return "Fruit TinyShape";
+}
+string TinyShape::getSpecific(){
+	return "TinyShape";
+}
+//fruits kinds
+//grape
+string Grapes::getType(){
+	return OvalShape::getType()+" "+TinyShape::getType()+" "+"Grape";
+}
+string Grapes::getSpecific(){
+	return "Grape";
+}
+string Grapes::getTaste(){
+	return "<I'm sweet and juicy>";
+}
+//berry
+string Berry::getType(){
+	return OvalShape::getType()+" "+TinyShape::getType()+" "+"Berry";
+}
+string Berry::getSpecific(){
+	return "Berry";
+}
+string Berry::getTaste(){
+	return "<I'm sour>";
+}
+//black berry
+string BlackBerry::getType(){
+	return Berry::getType()+" "+"BlackBerry";
+}
+string BlackBerry::getSpecific(){
+	return "BlackBerry";
+}
+//blue berry
+string BlueBerry::getType(){
+	return Berry::getType()+" "+"BlueBerry";
+}
+string BlueBerry::getSpecific(){
+	return "BlueBerry";
+}
+//goose berry
+string GooseBerry::getType(){
+	return Berry::getType()+" "+"GooseBerry";
+}
+string GooseBerry::getSpecific(){
+	return "GooseBerry";
+}
+//eleder berry
+string ElderBerry::getType(){
+	return Berry::getType()+" "+"ElderBerry";
+}
+string ElderBerry::getSpecific(){
+	return "ElderBerry";
+}
+//strawberry
+string Strawberry::getType(){
+	return Berry::getType()+" "+"Strawberry";
+}
+string Strawberry::getSpecific(){
+	return "Strawberry";
+}
+string Strawberry::getTaste(){
+	return "<I'm sweet>";
+}
+//avocado
+string Avocado::getType(){
+	return OvalShape::getType()+" "+"Avocado";
+}
+string Avocado::getSpecific(){
+	return "Avocado";
 }
 
-
+string Avocado::getTaste(){
+	return "<I'm creamy>";
+}
+//apple
+string Apple::getType(){
+	return OvalShape::getType()+" "+"Apple";
+}
+string Apple::getSpecific(){
+	return "Apple";
+}
+string Apple::getTaste(){
+	return "<I'm sweet>";
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 void Tree ::Iterate()
+{
+   IterateHelper("Fruit");	
+}
+void Tree ::IterateHelper(string type)
 {
    // list <int> myList;
     if(root==NULL) {
     		 return ;
     	 }
     	stack<Fruit*> stack;
-    	Fruit* currentNode=root;
-    	while (!stack.empty() || currentNode!=NULL ) {
-    		while(currentNode!=NULL) {
-    			stack.push(currentNode);
-    			currentNode=currentNode->getLeft();
+    	Fruit* currentFruit=root;
+		cout<<"the "<<type<<" : ";
+    	while (!stack.empty() || currentFruit!=NULL ) {
+    		while(currentFruit!=NULL) {
+    			stack.push(currentFruit);
+    			currentFruit=currentFruit->getLeft();
     		}
-    		currentNode=stack.top();
-			cout<< currentNode->getWeight()<<" --> ";
+    		currentFruit=stack.top();
+			
+			if(currentFruit->getType().find(type)!=string::npos){
+		   	cout<< currentFruit->getWeight()<<" --> ";			
+			}
 			stack.pop();
-    		currentNode=currentNode->getRight();    		    		
+    		currentFruit=currentFruit->getRight();    		    		
     	}   
 		cout<<"done \n";
 		
 }
+void Tree::filterByType(string type){
+	IterateHelper(type);
+}
+
+Fruit* Tree::InsertHelper(string type,int value){
+
+   if(type.compare("Grape")==0){
+     Grapes* grape=new Grapes();
+	 grape->setWeight(value);
+     return  grape;   
+   }
+    else if(type.compare("Berry")==0){
+	Berry* berry=new Berry();
+	berry->setWeight(value);
+    return berry;
+   }
+    else if(type.compare("BlackBerry")==0){
+	BlackBerry* berry=new BlackBerry();
+	 berry->setWeight(value);
+    return berry;
+   }
+   else if(type.compare("BlueBerry")==0){
+	BlueBerry* berry=new BlueBerry();
+	 berry->setWeight(value);
+    return berry;
+   }
+   else if(type.compare("GooseBerry")==0){
+	GooseBerry* berry=new GooseBerry();
+	 berry->setWeight(value);
+    return berry;
+   }
+   else if(type.compare("ElderBerry")==0){
+	ElderBerry* berry=new ElderBerry();
+	 berry->setWeight(value);
+    return berry;
+   }
+   else if(type.compare("Strawberry")==0){
+	Strawberry* berry=new Strawberry();
+	 berry->setWeight(value);
+    return berry;
+   }
+   else if(type.compare("Avocado")==0){
+	Avocado* fruit=new Avocado();
+	 fruit->setWeight(value);
+    return fruit;
+   }
+   else if(type.compare("Apple")==0){
+	Apple* fruit=new Apple();
+	fruit->setWeight(value);
+    return fruit;
+   }
+   else if(type.compare("OvalShape")==0){
+	OvalShape* shape=new OvalShape();
+	shape->setWeight(value);
+    return shape;
+   }
+   else if(type.compare("TinyShape")==0){
+	TinyShape* shape=new TinyShape();
+	shape->setWeight(value);
+    return shape;
+   }
+
+
+   return new Fruit(value);
+  
+}
+
+
 // Insert function definition.
-Fruit* Tree ::Insert(Fruit* node, int value)
+Fruit* Tree ::Insert(Fruit* parent,Fruit* node,string type, int value)
 {
     if (!node)
-    {
-        // Insert the first node, if root is NULL.
-	//	Fruit* root;
-        return new Fruit(value);
+    {	
+        return InsertHelper(type,value);
     }
- 
     // Insert data.
     if (value > node->getWeight())
     {
-        // Insert right node data, if the 'value'
-        // to be inserted is greater than 'root' node data.
- 
-        // Process right nodes.
-        node->rightChild=Insert(node->getRight(), value);
+        node->setRight(Insert(node,node->getRight(),type, value));
     }
-    else
+    else if(value < node->getWeight())
     {
-        // Insert left node data, if the 'value'
-        // to be inserted is greater than 'root' node data.
- 
-        // Process left nodes.
-        node->leftChild=Insert(node->getLeft(), value);
+          node->setLeft(Insert(node,node->getLeft(),type, value));
     }
- 
-    // Return 'root' node, after insertion.
+	else if(value == node->getWeight()){
+        Fruit* fruit=InsertHelper(type,value);
+		fruit->setLeft(node->getLeft());
+		fruit->setRight(node->getRight());
+		if(parent==NULL){
+           root=fruit;
+		   return root;
+		}
+		if(parent->getLeft()==node){
+			parent->setLeft(fruit);
+		}
+		else{
+			parent->setRight(fruit);
+		}
+		return fruit;
+	}
     return node;
 }
 Fruit* Tree::findHeaviest(){
@@ -85,22 +274,22 @@ Fruit* Tree::findHeaviest(){
     		 return NULL ;
     	 }
     	
-    	Fruit* currentNode=root;
-    		while(currentNode->getRight()!=NULL) {
-    			currentNode=currentNode->getRight();
+    	Fruit* currentFruit=root;
+    		while(currentFruit->getRight()!=NULL) {
+    			currentFruit=currentFruit->getRight();
     		}  
-			return currentNode;
+			return currentFruit;
 } 
 Fruit* Tree::findLightest(){
 if(root==NULL) {
     		 return NULL;
     	 }
     	
-    	Fruit* currentNode=root;
-    		while(currentNode->getLeft()!=NULL) {
-    			currentNode=currentNode->getLeft();
+    	Fruit* currentFruit=root;
+    		while(currentFruit->getLeft()!=NULL) {
+    			currentFruit=currentFruit->getLeft();
     		} 
-			return currentNode; 
+			return currentFruit; 
 } 
 void Tree::filterByWeight(int weight){
 	list <Fruit*> weightList;
@@ -108,46 +297,200 @@ void Tree::filterByWeight(int weight){
     		 return ;
     	 }
     	stack<Fruit*> stack;
-    	Fruit* currentNode=root;
-    	while (!stack.empty() || currentNode!=NULL ) {
-    		while(currentNode!=NULL) {
-    			stack.push(currentNode);
-    			currentNode=currentNode->getRight();
+    	Fruit* currentFruit=root;
+    	while (!stack.empty() || currentFruit!=NULL ) {
+    		while(currentFruit!=NULL) {
+    			stack.push(currentFruit);
+    			currentFruit=currentFruit->getRight();
     		}
-    		currentNode=stack.top();
-			if(currentNode->getWeight()>weight){
-				weightList.push_front(currentNode);
+    		currentFruit=stack.top();
+			if(currentFruit->getWeight()>weight){
+				weightList.push_front(currentFruit);
 			}
 			else{
 				int size=weightList.size();
                 for(int it = 0; it<size; it++){
-			        cout << weightList.front()->getWeight()<< " ---> ";
+			        cout << "type:"<<weightList.front()->getType()<<"->"<<weightList.front()->getWeight()<< " ---> ";
 				    weightList.pop_front();
 				}
-				cout<<"done";
+				cout<<"done \n";
 				return;
 			}
 			stack.pop();
-    		currentNode=currentNode->getLeft();    		    		
+    		currentFruit=currentFruit->getLeft();    		    		
     	}   	
 
 }
 
 
+  Fruit* Tree::findSuccessor(Fruit* node, Fruit* suc,int key) {
+       
+        if (node==NULL) {
+            return suc;
+        }
+        if (node->getWeight()==key) {
+            if (node->getRight()!=NULL) {
+                Fruit* currentNode = node->getRight();
+                while (currentNode->getLeft()!=NULL) {
+                    currentNode = currentNode->getLeft();
+                }
+                suc = currentNode;
+            }
+			//parent->setLeft(suc->getRight());
+            return suc;
+        } else if (node->getWeight()>key) {
+            suc = node;
+            return findSuccessor(node->getLeft(), suc,key);
+        } else {
+            return findSuccessor(node->getRight(), suc, key);
+        }
+    }
+	/////////////////////////////////////
+	Fruit* Tree::getParent(){
+		return this->parent;
+	}
+	void Tree::setParent(Fruit* p){
+		 this->parent=p;
+	}
+	//////////////////////////////////////////////
+	Fruit* Tree::searchForNode(Fruit* parent,Fruit* node, int key) {
+
+        if (node==NULL) {
+            return NULL;
+        }
+        if (node->getWeight()==key) {
+		   setParent(parent);
+            return node;
+        } else if (key>node->getWeight()) {
+            return searchForNode(node,node->getRight(), key);
+        } else {
+            return searchForNode(node,node->getLeft(), key);
+        }
+    }
+
+Fruit* Tree::deleteFruit(int key) {
+      
+      //search function
+        Fruit* node = searchForNode(new Fruit(),root, key);
+        if (node == NULL) return NULL;
+        //step1
+        if (node->getLeft()==NULL) { 
+			Fruit* parent=getParent();         
+            if(parent->getLeft()==node){
+				parent->setLeft(node->getRight());
+			}
+			else{
+				parent->setRight(node->getRight());
+			}
+        }
+        //step2
+        else if (node->getRight()==NULL) {
+            Fruit* parent=getParent();         
+            if(parent->getLeft()==node){
+				parent->setLeft(node->getLeft());
+			}
+			else{
+				parent->setRight(node->getLeft());
+			}
+        }
+        //step3
+        else {
+			Fruit* point=new Fruit();
+            Fruit* newNode = findSuccessor(node,point,key);
+			deleteFruit(newNode->getWeight());
+			Fruit* parent=getParent();
+			newNode->setLeft(node->getLeft());
+            newNode->setRight(node->getRight());
+			if(node==root){
+              root=newNode;
+		      return root;
+		    }
+			if(parent->getLeft()==node){
+				parent->setLeft(newNode);
+			}
+			else{
+				parent->setRight(newNode);
+			}
+            
+           
+        }
+        
+        return node;
+    }
+	
+
+void Tree:: magnifyByType(string type, int value){
+    stack<Fruit *> nodes;
+    Fruit *currentFruit = root;
+    list <Fruit*>  fruits;
+    while (currentFruit != NULL || nodes.empty() == false)
+    {
+        while (currentFruit !=  NULL)
+        {
+            nodes.push(currentFruit);
+            fruits.push_back(currentFruit);
+            currentFruit = currentFruit->getLeft();
+        }
+        currentFruit = nodes.top();
+        nodes.pop();
+        currentFruit = currentFruit->getRight();
+
+    }
+    while(! fruits.empty()){
+        Fruit* current= fruits.front();
+        int newWeight = (current->getWeight()) + value;
+        if(current->getType().find(type)!= string::npos){
+            Fruit* deleted= deleteFruit(current->getWeight());
+            Insert(NULL,root,deleted->getSpecific(),newWeight);
+        }
+        fruits.pop_front();
+    }
+
+    Iterate();
+}
+/*void Tree::levelOrder(Fruit* root) {
+       list <Fruit*>  fruits;
+        fruits.push_front(root);
+        int index=0;
+        while(index<fruits.size() ){
+            Fruit* node=fruits.(index++);
+                          
+                if (node.left!= null) {
+                           arr.add(node.left);
+                }
+                   if (node.right!= null) {
+                           arr.add(node.right);
+                }
+              
+            
+            
+        }
+        for(int i=0;i<fruits.size();i++){
+            System.out.print(fruits.get(i).data+" ");
+        }
+      
+    }*/
 int main() { 
 Tree b;
-root= b.Insert(root, 50); 
-b.Insert(root, 30); 
-b.Insert(root, 20);
- b.Insert(root, 40);
-  b.Insert(root, 70); 
- b.Insert(root, 60); 
- //b.Insert(root, 50);
-  b.Iterate();
-  Fruit* min=b.findLightest();
-  cout<<" min is "<<min->getWeight()<<"\n";
-  Fruit* max=b.findHeaviest();
-  cout<<" max is "<<max->getWeight()<<"\n";
-  b.filterByWeight(20);
-  return 0;
+root=b.Insert(NULL,root,"Grape", 50); 
+b.Insert(NULL,root,"Apple", 60);
+b.Insert(NULL,root,"Grape", 30); 
+b.Insert(NULL,root,"Grape", 40); 
+b.Insert(NULL,root,"Grape", 10);
+		      
+
+b.Iterate();	
+
+b.deleteFruit(50);
+b.Iterate();
+Fruit* min=b.findLightest();
+//cout<<" min : type is :"<<min->getWeight()<< "  and  " <<min->getWeight()<<"\n";
+Fruit* max=b.findHeaviest();
+
+cout<<" max is "<<max->getType()<<"\n";
+b.filterByWeight(20);
+b.filterByType("Berry");
+b.magnifyByType("Grape",100);
+
+return 0;
    } 
